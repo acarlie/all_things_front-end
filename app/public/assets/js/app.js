6e2f23c8e59f8651c68a3070b2a0341a9dff17fa
acarlie <ameliacarlie@gmail.com>
2019-10-31 15:39:49 -0400

    Added gradient underline effects

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
         const isOpen = $(this).data('open');
         const id = $(this).attr('data-id');
         if (isOpen) {
+            $(`#article-${id}`)
+                .removeClass('article--open');
             $(`#toggler-${id}`)
                 .addClass('article__toggler--closed')
                 .removeClass('article__toggler--open');
@@ -12,6 +14,8 @@ $(document).ready(function () {
                 .removeClass('article__content-toggler--visible');
             $(this).data('open', false);
         } else {
+            $(`#article-${id}`)
+                .addClass('article--open');
             $(`#toggler-${id}`)
                 .addClass('article__toggler--open')
                 .removeClass('article__toggler--closed');
